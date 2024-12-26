7f19ad476111c670a227434f7d0b38e07e56fa87
seanli <seanli9018@gmail.com>
2024-12-25 23:28:43 -0600

    fixed react spring animated type issue

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -7,7 +7,6 @@ export default function Page() {
       <Header />
       <main className="flex flex-col gap-8 items-center justify-stretch flex-1 max-w-screen-2xl mx-auto">
         <div className="py-4 px-6 w-full">
-          <h1>Hello, this is a card game list!</h1>
           <div className=" grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             <Tile
               title="Yes or No?"
@@ -18,12 +17,6 @@ export default function Page() {
                 href: "/games/yes-no",
                 buttonType: "link",
               }}
-            />
-            <Tile
-              title="Yes or No? Yes or No? Yes or No? Yes or No? Yes or No?"
-              description="Choose a card out of 2 cards, and it will either tell you 'Yes' or 'No' Choose single card, and it will either tell you 'Yes' or 'No' Choose single card, and it will either tell you 'Yes' or 'No'"
-              tags={["Single player", "Single device"]}
-              cta={{ children: "Play", onClick: () => console.log("clicked") }}
             />
           </div>
         </div>
--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,3 +1,21 @@
+import { animated as originalAnimated } from "@react-spring/web";
+
+declare module "@react-spring/web" {
+  // Extend animated components
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface AnimatedProps<T> {
+    myCustomProp?: string; // Add your custom property here
+  }
+
+  type ExtendedAnimated = typeof originalAnimated & {
+    div: React.ComponentType<
+      React.HTMLProps<HTMLDivElement> & AnimatedProps<{ children: Element }>
+    >;
+  };
+
+  export const animated: ExtendedAnimated;
+}
+
 declare module "lodash/omit" {
   import { Omit } from "lodash";
   export default Omit;
