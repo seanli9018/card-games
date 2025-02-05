c01a983d95360b01b87ef61b5ff26139d1ab0769
Yuxiang Li <yuxiangli@Mac.attlocal.net>
2025-02-04 19:53:52 -0600

    try to fix card auto reveal

--- src/components/shared/cards-layout/cards-flex/cardsFlex.tsx
+++ src/components/shared/cards-layout/cards-flex/cardsFlex.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card } from '../..';
 import { oneLinerShuffleArray, fisherYatesShuffleArray } from '@/utils';
 import type { CardsLayoutProps } from '../cardsLayout.type';
